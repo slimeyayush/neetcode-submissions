cf79a55169e3b33afc700b6c0a2c82df6536c095
slimeyayush <96426902+slimeyayush@users.noreply.github.com>
2026-07-12 00:58:52 +0530

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int[] indegree = new int[numCourses];
+        List<List<Integer>> adjList = new ArrayList<>();
+        for(int i=0; i<numCourses; i++) {
+            adjList.add(new ArrayList<>());
+        }
+        for(int[] pre:prerequisites) {
+            int toDo = pre[0];
+            int preq = pre[1];
+            adjList.get(preq).add(toDo);
+            indegree[toDo]++;
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=0; i<numCourses; i++) {
+               if(indegree[i] == 0) {
+                queue.offer(i);
+               }
+        }
+        List<Integer> canFinish = new ArrayList<>();
+        while(!queue.isEmpty()) {
+            int finished = queue.poll();
+            canFinish.add(finished);
+            for(int neigh: adjList.get(finished)) {
+                indegree[neigh]--;
+                if(indegree[neigh] == 0) {
+                    queue.offer(neigh);
+                }
+            }
+        }
+        int size = canFinish.size();
+        int[] res = new int[size];
+        for(int i=0; i<size; i++) {
+            res[i] = canFinish.get(i);
+        }
+        return size == numCourses? res:new int[0];
+    }
+}
